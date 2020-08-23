d1a5d06f848e4349e8d6f9c960f4c46e24f5ce77
Jay Cincotta <jay.cincotta@gmail.com>
2020-08-22 18:08:31 -0700

    Integrating service account

--- TestFunctions.cs
+++ TestFunctions.cs
@@ -11,13 +11,14 @@ using System.Collections.Specialized;
 using System.Runtime.InteropServices.ComTypes;
 using System.Collections.Generic;
 using System.Linq;
+using StarVoteServer.Google;
 
 namespace StarVote
 {
     public static class TestFunctions
     {
         [FunctionName(nameof(Ping))]
-        public static async Task<IActionResult> Ping(
+        public static IActionResult Ping(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
@@ -29,9 +30,26 @@ namespace StarVote
                 ? "Hi!"
                 : $"Hello, {name}!";
 
+            var acct = new ServiceAccount();
+            log.LogInformation(acct.Info);
             return new OkObjectResult(responseMessage);
         }
 
+        [FunctionName(nameof(ReadRange))]
+        public static IActionResult ReadRange(
+    [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+    ILogger log)
+        {
+            log.LogInformation($"{nameof(ReadRange)}({Expand(req.Query)})");
+
+            string doc = req.Query["doc"];
+            string sheet = req.Query["sheet"];
+            string range = req.Query["range"];
+
+            string responseMessage = $"Read {range} from {sheet} in {doc}";
+
+            return new OkObjectResult(responseMessage);
+        }
         static string Expand(IQueryCollection collection)
         {
             var list = new List<string>();
